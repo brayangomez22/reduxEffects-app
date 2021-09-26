ebc16c0941479cf4083d0ec27495057f7152dd41
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-26 13:28:35 -0500

    Routes of our application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListComponent } from './users/list/list.component';
+import { UserComponent } from './users/user/user.component';
+
+const routes: Routes = [
+  { path: 'home', component: ListComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goUser(id: string) {
+    if (!id) {
+      return;
+    }
+
+    this.router.navigate(['/user', id]);
   }
-
 }
